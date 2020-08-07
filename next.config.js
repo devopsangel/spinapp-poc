ef576b87866845b7dec19bd2e0983a6766af705c
Askar Kengesbayev <akengesbayev@peak6.com>
2020-08-07 15:19:58 -0400

    testing new config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const webpack = require('webpack');
 
-const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
+// const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 
 module.exports = {
     webpack: (config) => {
-        const env = { API_KEY: apiKey };
-        config.plugins.push(new webpack.DefinePlugin(env));
+        // const env = { API_KEY: apiKey };
+        // config.plugins.push(new webpack.DefinePlugin(env));
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                API_KEY: JSON.stringify(process.env.SHOPIFY_API_KEY),
+            }),
+        );
         return config;
     },
 };
